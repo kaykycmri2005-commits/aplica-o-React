0e7b24c5f3da7dd0c48a18a35d8fc02ce168622e
KaykyCmri <kaykycmri2005@gmail.com>
2026-03-31 20:58:40 -0300

    Add App component with CardProduto integration

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import CardProduto from "./components/CardProduto/CardProduto";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <CardProduto />
+    </div>
+  );
+}
+
+export default App;
